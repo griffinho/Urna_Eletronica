7f5cd207ded191b6b169a933e73718d3d028a522
Erik Caue <89105395+griffinho@users.noreply.github.com>
2021-09-21 20:37:30 -0300

    Add files via upload

---
+++ Urna Eletronica/Projeto06/js/etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '11111',
+                nome: 'Tonho',
+                partido: 'PTC',
+                fotos:[
+                    {url: 'V1.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '22222',
+                nome: 'Bahia',
+                partido: 'BHA',
+                fotos:[
+                    {url: 'V2.jpg', legenda:'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '99',
+                nome: 'Raimuldo',
+                partido: 'XYZ',
+                vice: 'Tonho',
+                fotos:[
+                    {url:'P1.jpg', legenda: 'Prefeito'},
+                    {url:'Vp1.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '84',
+                nome: 'Cleytão',
+                partido: 'ABC',
+                vice: 'Carlão',
+                fotos:[
+                    {url:'P2.jpg', legenda: 'Prefeito'},
+                    {url:'Vp2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+        ]
+    }
+]
